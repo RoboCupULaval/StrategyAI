6e9c9d920841df29bd1cbc9d8658ff3d0cb5d613
jbecirovski <julien@becirovski.com>
2015-05-29 14:07:19 -0400

    add STP, BlackBoard, InfoManager, Executors, Strategy sequence

---
+++ UltimateStrat/Executor/SkillExecutor.py
@@ -0,0 +1,38 @@
+from UltimateStrat.Executor.Executor import Executor
+
+__author__ = 'jbecirovski'
+
+
+class SkillExecutor(Executor):
+    """
+    SkillExecutor is a sequence of request that select next pose for each players
+    1 - what's player skill ?
+    2 - what's player target ?
+    3 - what's player goal ?
+    4 - get skill object
+    5 - generate next pose
+    6 - set next pose
+    """
+    def __init__(self, info_manager):
+        Executor.__init__(self, info_manager)
+
+    def exec(self):
+        # Execution for each players
+        for id_player in range(self.info_manager.getCountPlayer()):
+            # 1 - what's player skill ?
+            current_skill = self.info_manager.getPlayerSkill(id_player)
+
+            # 2 - what's player target ?
+            current_target = self.info_manager.getPlayerTarget(id_player)
+
+            # 3 - what's player goal ?
+            current_goal = self.info_manager.getPlayerGoal(id_player)
+
+            # 4 - get skill object
+            skill = self.skill_book[current_skill]
+
+            # 5 - generate next pose
+            next_pose = skill.act(current_target, current_goal)
+
+            # 6 - set next pose
+            self.info_manager.setPlayerNextPose(id_player, next_pose)
---
+++ UltimateStrat/unittest/test_infoManager.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+__author__ = 'jbecirovski'
+
+
+class TestInfoManager(TestCase):
+  def test_getCurrentPlay(self):
+    self.fail()
+
+  def test_getCurrentPlaySequence(self):
+    self.fail()
+
+  def test_setPlay(self):
+    self.fail()
+
+  def test_initPlaySequence(self):
+    self.fail()
+
+  def test_incPlaySequence(self):
+    self.fail()
+
+  def test_getPlayerTarget(self):
+    self.fail()
+
+  def test_getPlayerGoal(self):
+    self.fail()
+
+  def test_getPlayerSkill(self):
+    self.fail()
+
+  def test_getPlayerTactic(self):
+    self.fail()
+
+  def test_getCountPlayer(self):
+    self.fail()
+
+  def test_setPlayerSkillTargetGoal(self):
+    self.fail()
+
+  def test_setPlayerTactic(self):
+    self.fail()
+
+  def test_setPlayerNextPose(self):
+    self.fail()
+
+  def test_getNextState(self):
+    self.fail()
+
+  def test_getNextPlay(self):
+    self.fail()
