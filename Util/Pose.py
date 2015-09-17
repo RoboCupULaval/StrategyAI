75bfa43ce9fde81d1407d0bb35664a0fd9efe8ae
Félix Pelletier <felixpelletier23@gmail.com>
2015-09-17 18:02:03 -0400

    Removed orientation limitation assert in Pose

--- Util/Pose.py
+++ Util/Pose.py
@@ -6,10 +6,9 @@ class Pose(object):
     def __init__(self, position=Position(), orientation=0.0):
         assert(isinstance(position, Position)), 'position should be Position object.'
         assert(isinstance(orientation, (int, float))), 'orientation should be int or float value.'
-        assert(0 <= orientation <= 360), 'orientation should be between 0 and 360 degrees'
 
         self.position = position
-        self.orientation = orientation
+        self.orientation = orientation % 360
 
     def __str__(self):
         return '[{}, theta={}]'.format(self.position, self.orientation)
