42fb19e1470ba76701cdf46d2d13c047c4e050e6
Félix Pelletier <felixpelletier23@gmail.com>
2016-01-24 15:48:58 -0500

    Added ball velocity

--- Game/Ball.py
+++ Game/Ball.py
@@ -1,6 +1,25 @@
 from ..Util.Position import Position
+import time
 
 
 class Ball():
     def __init__(self):
-        self.position = Position()
+        self._position = Position()
+        self._last_time = time.time()
+        self.velocity = Position()
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, pos):
+        if pos != self._position:
+            newtime = time.time()
+            deltatime = newtime-self._last_time
+            self.velocity.x = (pos.x - self._position.x)/deltatime
+            self.velocity.y = (pos.y - self._position.y)/deltatime
+
+            self._position = pos
+            self._last_time = newtime
+
