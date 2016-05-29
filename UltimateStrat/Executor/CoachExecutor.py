ae1353c13a75c41cd179f7e8d71705f3e369b9b5
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2016-05-29 17:44:37 -0400

    fix test_coachExecutor

--- UltimateStrat/Executor/CoachExecutor.py
+++ UltimateStrat/Executor/CoachExecutor.py
@@ -1,4 +1,5 @@
-#Under MIT License, see LICENSE.txt
+# Under MIT License, see LICENSE.txt
+""" Cet Executor détermine selon les informations du GameState le prochain Play. """
 from UltimateStrat.Executor.Executor import Executor
 
 
@@ -6,30 +7,30 @@ __author__ = 'RoboCupULaval'
 
 
 class CoachExecutor(Executor):
-    """
-    CoachExecutor is a sequence of requests that select Play for game :
-    1 - what's new state ?
-    2 - what's current play
-    3 - what's play with new state ?
-    4 - compare current play ans generate play
-    5 - set play and init sequence if play is not same
+    """ CoachExecutor est une série de requêtes pour choisir un Play.
+
+    1 - Quel est le nouvel état?
+    2 - Quel est le Play actuel?
+    3 - Quel devrait être le nouveau Play?
+    4 - Générer le Play selon le Play choisie et le GameState actuel.
+    5 - Assigner le Play s'il est différent que l'actuel.
     """
     def __init__(self, info_manager):
         Executor.__init__(self, info_manager)
 
     def exec(self):
         # 1 - what's current state ?
-        state = self.info_manager.getNextState()
+        state = self.info_manager.get_next_state()
 
         # 2 - what's current play
-        current_play = self.info_manager.getCurrentPlay()
+        current_play = self.info_manager.get_current_play()
 
         # 3 - what's play with state ?
-        play = self.info_manager.getNextPlay(state)
+        play = self.info_manager.get_next_play(state)
 
         # 4 - compare current play and generate play
         if not current_play == play:
             # 5 - set play and init sequence if play is not same
-            self.info_manager.setPlay(play)
-            self.info_manager.initPlaySequence()
+            self.info_manager.set_play(play)
+            self.info_manager.init_play_sequence()
 
--- tests/Executors/test_coachExecutor.py
+++ tests/Executors/test_coachExecutor.py
@@ -1,9 +1,33 @@
 # Under MIT License, see LICENSE.txt
-from unittest import TestCase
+""" Module de test pour CoachExecutor, nosetests style """
+from nose.tools import assert_equal
+from UltimateStrat.InfoManager import InfoManager
+from UltimateStrat.Executor.CoachExecutor import CoachExecutor
+from RULEngine.Game import Ball, Field, Player, Team
 
 __author__ = 'RoboCupULaval'
 
 
-class TestCoachExecutor(TestCase):
+class TestCoachExecutor:
+    """ Class pour CoachExecutor """
+
+    @classmethod
+    def setup(cls):
+        """ Setup """
+        players = []
+        for i in range(6):
+            players.append(Player.Player(i))
+
+        cls.info_manager = InfoManager(Field.Field(Ball.Ball()),
+                                       Team.Team(players, True),
+                                       Team.Team(players, False))
+        cls.coach = CoachExecutor(cls.info_manager)
+
     def test_exec(self):
-        self.fail()
+        """ Test basic de exec, le prochain play et le play actuel ne concorde
+            pas.
+        """
+        # FIXME: actuellement get_next_play retourne un play fix
+        self.info_manager.set_play('pHalt')
+        self.coach.exec()
+        assert_equal(self.info_manager.get_current_play(), 'pTestBench')
