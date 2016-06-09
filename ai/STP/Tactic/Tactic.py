7f6ea20ff08e7832fa053a13d06e9d2663949c61
Alexandra Mercier <alexandra.mercier.101@gmail.com>
2016-06-08 23:58:42 -0400

    Baesclass tactic implementer

--- ai/STP/Tactic/Tactic.py
+++ ai/STP/Tactic/Tactic.py
@@ -4,11 +4,11 @@ __author__ = 'Robocup ULaval'
 
 from abc import abstractmethod
 from functools import wraps
-from UltimateStrat import InfoManager
-from Util import geometry
-from UltimateStrat.STP.Skill import Action
+from ... import InfoManager
+from ..Skill import Stop
 
-class Tactique :
+
+class Tactique:
     """
     Classe mère de toutes les tactiques
     méthodes:
@@ -21,19 +21,23 @@ class Tactique :
         current_state : chcîne de caratères définissant l'état courant
         next_state : chcîne de caratères définissant l'état suivant
     """
+
     def __init__(self, info_manager, team_id, player_id):
         self.info_manager = info_manager
         self.team_id = team_id
         self.player_id = player_id
-        self.current_state = "halt"
-        self.next_state = "halt"
-
-    def exec(self):
-        self.dispatch()
+        self.current_state = 'halt'
+        self.next_state = 'halt'
+        self.dispatch = {'halt': halt}
 
     def halt(self):
-        self.next_state = "halt"
+        arret = Stop(self.info_manager, self.player_id)
+        self.next_state = 'halt'
+        return arret
 
-    def dispatch(self):
+    def exec(self):
+        # Trouver la prochaine action
+        next_action = self.dispatch[self.current_state]
+        next_pose = next_action.exec()
+        self.info_manager.set_player_next_action(self.player_id, next_pose)
         self.current_state = self.next_state
-
