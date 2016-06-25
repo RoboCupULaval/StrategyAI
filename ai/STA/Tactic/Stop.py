6daa512e0b9065d1ccd2602ab485094c5ca7b82e
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2016-06-25 03:12:41 -0400

    Ajout des nouveau modules StrategyBook et Tactic.Stop

---
+++ ai/STA/Tactic/Stop.py
@@ -0,0 +1,9 @@
+# Under MIT license, see LICENSE.txt
+
+from .Tactic import Tactic
+from . import tactic_constants
+
+class Stop(Tactic):
+    def __init__(self, p_info_manager):
+        super().__init__(p_info_manager)
+        self.status_flag = tactic_constants.SUCCESS
