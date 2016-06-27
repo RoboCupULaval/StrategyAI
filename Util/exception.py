ae8db1f1d220a748c9bc259ccd9a6550d771686b
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2016-06-26 20:59:29 -0400

    retrait exceptions et constantes

--- Util/exception.py
+++ Util/exception.py
@@ -3,17 +3,9 @@
     artificielle et des stratégies.
 """
 
-class InvalidDebugType(Exception):
-    """ Est levée si un paquet de débogage n'a pas le bon type. """
-    pass
-
 class StopPlayerError(Exception):
     """
         Est levée si le moteur est incapable d'envoyer la commande pour arrêter
         les joueurs.
     """
-    pass
-
-class NonExistentModule(Exception):
-    """ Est levé par l'InfoManager quand un module intelligent n'existe pas. """
     pass
