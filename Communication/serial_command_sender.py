79eaae40a0f6a8e463ec5e0a5e68ecf6bca928c2
Robocup ULaval <robocupuniversitelaval@gmail.com>
2016-08-03 20:10:25 -0400

    Fixed serial_command sender when using ttyACM

--- Communication/serial_command_sender.py
+++ Communication/serial_command_sender.py
@@ -1,5 +1,6 @@
 #Under MIT License, see LICENSE.txt
 #!/usr/bin/python
+import serial
 from . import serial_protocol as protocol
 import math
 import os
@@ -9,9 +10,13 @@ class SerialCommandSender(object):
     
     def __init__(self, port=None, baudRate=115200):
         if not port:
-            serial_ports = [port for port in os.listdir('/dev') if port.startswith("ttyUSB")]
-            port = serial_ports[0]
-        
+            serial_ports = [port for port in os.listdir('/dev')
+                            if port.startswith("ttyUSB") or port.startswith('ttyACM')]
+            try:
+                port = serial_ports[0]
+            except IndexError:
+                raise Exception('No suitable serial port found.')
+            
         self.serial = serial.Serial('/dev/' + port, baudRate)
         self.last_time = time.time()
         
