46f11758b172e66e346350fff521a9ea2572a0e6
Alexandra Mercier <alexandra.mercier.101@gmail.com>
2016-09-14 19:58:51 -0400

    ajout des getters pour le GameStateManager

--- ai/GameStateManager.py
+++ ai/GameStateManager.py
@@ -37,11 +37,11 @@ class GameStateManager:
 
     """
         Met à jour les informations du joueur
-        :param is_my_team: Booléen, l'équipe du joueur est mon équipe
+        :param is_my_team: Booléen avec valeur vrai par défaut, l'équipe du joueur est mon équipe
         :param player_id: identifiant du joueur, en int
         :param player_pose: Nouvelle Pose à donner au joueur
     """
-    def _update_player(self, is_my_team, player_id, player_pose):
+    def _update_player(self, player_id, player_pose, is_my_team=True):
         if is_my_team:
             self.my_team.move_and_rotate_player(player_id, player_pose)
         else:
@@ -49,10 +49,10 @@ class GameStateManager:
 
     """
         Met à jour une équipe
-        :param is_my_team: Booléen, l'équipe du joueur est mon équipe
+        :param is_my_team: Booléen avec valeur vrai par défaut, l'équipe du joueur est mon équipe
         :param new_team_info: Team, info de l'équipe à mettre à jour
     """
-    def _update_team(self, is_my_team, new_team_info):
+    def _update_team(self, new_team_info, is_my_team=True):
         for i in range(PLAYER_PER_TEAM):
             self._update_player(is_my_team, i)
 
@@ -74,3 +74,22 @@ class GameStateManager:
         self._update_team(is_my_team, new_game_state.friends)
         self._update_team(not is_my_team, new_game_state.enemies)
         self._update_timestamp(new_game_state.timestamp)
+
+    """
+        Retourne la pose d'un joueur d'une équipe
+        :param is_my_team: Booléen avec valeur vrai par défaut, l'équipe du joueur est mon équipe
+        :param player_id: identifiant du joueur, en int
+        :return: La pose du joueur
+    """
+    def get_player_pose(self, player_id, is_my_team=True):
+        if is_my_team:
+            return self.my_team.players[player_id].pose
+        else:
+            return self.other_team.players[player_id].pose
+
+    """
+        Retourne la position de la balle
+        :return: la position de la balle
+    """
+    def get_ball_position(self):
+        return self.field.ball.position
