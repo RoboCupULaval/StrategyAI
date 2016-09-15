9c661646208e809c695372d5cf134b2e69f1ab4d
Alexandra Mercier <alexandra.mercier.101@gmail.com>
2016-09-14 20:07:35 -0400

    ajout du ModuleManager

---
+++ ai/ModuleManager.py
@@ -0,0 +1,17 @@
+
+class NonExistentModule(Exception):
+    """ Est levée si le module intelligent requis n'est pas enregistré. """
+    pass
+
+class ModuleManager:
+    def __init__(self):
+        self.modules = {}
+
+    def register_module(self, module_name, module_ref):
+        self.modules[module_name] = module_ref(self)
+
+    def acquire_module(self, module_name):
+        try:
+            return self.modules[module_name]
+        except KeyError:
+            raise NonExistentModule("Le module " + module_name + " n'existe pas.")
