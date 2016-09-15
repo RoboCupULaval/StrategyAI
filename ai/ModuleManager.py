ebf96e24b1d906aa6049d737eb08918a60428df3
Alexandra Mercier <alexandra.mercier.101@gmail.com>
2016-09-14 20:59:55 -0400

    essaie a conversion string-classe

--- ai/ModuleManager.py
+++ ai/ModuleManager.py
@@ -1,3 +1,4 @@
+from .Algorithm import PathfinderRRT
 
 class NonExistentModule(Exception):
     """ Est levée si le module intelligent requis n'est pas enregistré. """
@@ -5,9 +6,12 @@ class NonExistentModule(Exception):
 
 class ModuleManager:
     def __init__(self):
-        open('module_config', 'r')
-        
         self.modules = {}
+        f = open('module_config', 'r')
+        for module_name in f.read().split('\n'):
+            if hasattr(PathfinderRRT.modules[__name__], module_name):
+                module = getattr(object, module_name)
+                self.register_module(module_name, module)
 
     def register_module(self, module_name, module_ref):
         self.modules[module_name] = module_ref(self)
