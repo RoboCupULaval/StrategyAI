82ff03e216ab252ae9eec0d729bbe410134e1c2a
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2017-01-06 18:10:22 -0500

    margin 80!

--- ai/executors/debug_executor.py
+++ ai/executors/debug_executor.py
@@ -19,7 +19,8 @@ class DebugExecutor(Executor):
 
     def _execute_incoming_debug_commands(self):
         for command in self.ws.debug_state.from_ui_debug_commands:
-            self.ws.debug_state.transformed_ui_debug_commands.append(UIDebugCommand(command))
+            self.ws.debug_state.transformed_ui_debug_commands.\
+                append(UIDebugCommand(command))
 
         self._apply_incoming_debug_command()
 
@@ -29,8 +30,12 @@ class DebugExecutor(Executor):
 
     def _execute_outgoing_debug_commands(self):
         # todo make this work!
-        packet_represented_commands = [c.get_packet_repr() for c in self.ws.debug_state.from_ai_raw_debug_cmds]
-        self.ws.debug_state.to_ui_packet_debug_cmds = copy.deepcopy(packet_represented_commands)
+        packet_represented_commands = \
+            [c.get_packet_repr()
+             for c in self.ws.debug_state.from_ai_raw_debug_cmds]
+
+        self.ws.debug_state.to_ui_packet_debug_cmds = \
+            copy.deepcopy(packet_represented_commands)
         self.ws.debug_state.from_ai_raw_debug_cmds.clear()
 
     def _parse_command(self, cmd):
@@ -50,13 +55,17 @@ class DebugExecutor(Executor):
         strategy_key = cmd.data['strategy']
 
         if strategy_key == 'pStop':
-            self.ws.play_state.set_strategy(self.ws.play_state.get_new_strategy("DoNothing")(self.ws.game_state))
+            self.ws.play_state.set_strategy(self.ws.play_state.
+                                            get_new_strategy("DoNothing")
+                                            (self.ws.game_state))
 
         else:
-            self.ws.play_state.set_strategy(self.ws.play_state.get_new_strategy(strategy_key)(self.ws.game_state))
+            self.ws.play_state.set_strategy(self.ws.play_state.
+                                            get_new_strategy(strategy_key)
+                                            (self.ws.game_state))
 
     def _parse_tactic(self, cmd):
-        # TODO make implementation for other tactic packets! And finish this please
+        # TODO make implementation for other tactic packets!
         # FIXME this pid thingy is getting out of control
         player_id = self._sanitize_pid(cmd.data['id'])
         tactic_name = cmd.data['tactic']
@@ -64,11 +73,15 @@ class DebugExecutor(Executor):
         # TODO ui must send better packets back with the args.
         target = cmd.data['target']
         target = Pose(Position(target[0], target[1]))
-        tactic = self.ws.play_state.get_new_tactic('Idle')(self.ws.game_state, player_id, target)
+        tactic = self.ws.play_state.get_new_tactic('Idle')(self.ws.game_state,
+                                                           player_id,
+                                                           target)
         try:
-            tactic = self.ws.play_state.get_new_tactic(tactic_name)(self.ws.game_state, player_id, target)
+            tactic = self.ws.play_state.get_new_tactic(tactic_name)\
+                (self.ws.game_state, player_id, target)
         except:
-            print("La tactique n'a pas été appliquée par cause de mauvais arguments.")
+            print("La tactique n'a pas été appliquée par "
+                  "cause de mauvais arguments.")
 
         if isinstance(self.ws.play_state.current_strategy, HumanControl):
             hc = self.ws.play_state.current_strategy
