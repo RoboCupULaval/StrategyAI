23bf5a1b0872992019386707b98b3724c43e3073
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2017-02-01 16:35:26 -0500

    TESTS ARE FIXED - 4 TESTS SKIPPED

--- tests/Util/test_raycast.py
+++ tests/Util/test_raycast.py
@@ -5,6 +5,12 @@ from math import pi
 
 from ai.Util.Raycast import *
 from ai.states.game_state import GameState
+from RULEngine.Util.geometry import get_angle
+from RULEngine.Util.game_world import GameWorld
+from RULEngine.Game.Referee import Referee
+from RULEngine.Util.team_color_service import TeamColorService, TeamColor
+from RULEngine.Game.Game import Game
+from RULEngine.Game.Ball import Ball
 
 __author__ = 'RoboCupULaval'
 
@@ -13,7 +19,14 @@ __author__ = 'RoboCupULaval'
 class TestRaycast(unittest.TestCase):
     def setUp(self):
         self.game_state = GameState()
-        self.game_state._update_ball_position(Position(100, 0))
+        self.game = Game()
+        self.game.set_referee(Referee())
+        self.game.ball = Ball()
+        game_world = GameWorld(self.game)
+        game_world.set_team_color_svc(TeamColorService(TeamColor.YELLOW_TEAM))
+        self.game.set_our_team_color(TeamColor.YELLOW_TEAM)
+        self.game_state.set_reference(game_world)
+        self.game_state.game.ball.set_position(Position(100, 0), 0)
 
     def test_raycast(self):
         self.assertTrue(raycast(self.game_state, Position(100, 100), 200, -pi/2, BALL_RADIUS, [], [], False))
