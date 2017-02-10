8f338f38fcc7399b52837caf49f3386ce0075475
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2017-02-10 17:18:49 -0500

    Can access constants from field

--- ai/states/game_state.py
+++ ai/states/game_state.py
@@ -5,8 +5,10 @@
     Ce module garde en mémoire l'état du jeu
 """
 from RULEngine.Util.game_world import GameWorld
-
+from RULEngine.Util.constant import TeamColor
 from RULEngine.Util.singleton import Singleton
+from RULEngine.Util.Pose import Pose
+from RULEngine.Util.Position import Position
 
 
 class GameState(object, metaclass=Singleton):
@@ -21,14 +23,15 @@ class GameState(object, metaclass=Singleton):
         self.last_timestamp = 0
         self.debug_information_in = []
         self.ui_debug_commands = []
+        self.const = None
 
-    def get_our_team_color(self):
+    def get_our_team_color(self) -> TeamColor:
         return self.our_team_color
 
-    def get_my_team_player(self, player_id):
+    def get_my_team_player(self, player_id: int):
         pass
 
-    def get_player_pose(self, player_id, is_my_team=True):
+    def get_player_pose(self, player_id: int, is_my_team=True) -> Pose:
         """
             Retourne la pose d'un joueur d'une équipe
             :param is_my_team: Booléen avec valeur vrai par défaut, l'équipe du joueur est mon équipe
@@ -40,7 +43,7 @@ class GameState(object, metaclass=Singleton):
         else:
             return self.other_team.players[player_id].pose
 
-    def get_player_position(self, player_id, is_my_team=True):
+    def get_player_position(self, player_id: int, is_my_team=True) -> Position:
         """
             Retourne la position d'un joueur d'une équipe
             :param is_my_team: Booléen avec valeur vrai par défaut, l'équipe du joueur est mon équipe
@@ -52,7 +55,7 @@ class GameState(object, metaclass=Singleton):
         else:
             return self.other_team.players[player_id].pose.position
 
-    def get_ball_position(self):
+    def get_ball_position(self) -> Position:
         """
             Retourne la position de la balle
             :return: la position de la balle
@@ -66,7 +69,7 @@ class GameState(object, metaclass=Singleton):
         """
         return self.timestamp
 
-    def set_reference(self, world_reference):
+    def set_reference(self, world_reference: GameWorld):
         assert isinstance(world_reference, GameWorld), \
             "setting reference to the gamestate require an instance of RULEngine.Util.GameWorld"
         assert world_reference.game.referee is not None, \
@@ -79,3 +82,5 @@ class GameState(object, metaclass=Singleton):
         self.my_team = self.game.friends
         self.other_team = self.game.enemies
         self.our_team_color = world_reference.team_color_svc.OUR_TEAM_COLOR
+        self.const = self.game.field.constant
+        self.timestamp = world_reference.timestamp
