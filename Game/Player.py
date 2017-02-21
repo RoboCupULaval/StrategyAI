81d1ca9a5c99d540cf2ba0b791c6aacc06576bda
philippe lebel <philippe.lebel008@sympatico.ca>
2017-02-20 20:25:35 -0500

    player corrections

--- Game/Player.py
+++ Game/Player.py
@@ -21,8 +21,8 @@ class Player:
         control_input_model = [[0, 0, 0], [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 0], [0, 0, 1]]
         observation_model = [[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0]]
 
-        process_covariance = 10 ** (3) * np.eye(6)
-        observation_covariance = np.eye(3) * 10 ** (-3)
+        process_covariance = 10 ** (-2) * np.eye(6)
+        observation_covariance = np.eye(3) * 10 ** (0)
 
         initial_state_estimation = [self.pose.position.x, self.pose.position.y, 0, 0, self.pose.orientation, 0]
         initial_state_covariance = np.eye(6) * 1000
