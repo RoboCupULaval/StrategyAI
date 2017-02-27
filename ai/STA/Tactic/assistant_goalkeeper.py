c9b1555e61da8b77060cea1fef5e5a9e89cb9ea2
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2017-02-27 08:39:33 -0500

    ass goalkeeper

--- ai/STA/Tactic/assistant_goalkeeper.py
+++ ai/STA/Tactic/assistant_goalkeeper.py
@@ -1,5 +1,5 @@
 # Under MIT licence, see LICENCE.txt
-
+from ai.STA.Tactic.GoToPositionNoPathfinder import GoToPositionNoPathfinder
 from .Tactic import Tactic
 from ai.STA.Tactic.tactic_constants import Flags, DEFAULT_TIME_TO_LIVE
 from ..Action.ProtectGoal import ProtectGoal
@@ -19,19 +19,6 @@ __author__ = 'RoboCupULaval'
 # TODO WORK IN PROGRESS
 class AssistantGoalkeeper(Tactic):
     """
-    Tactique du gardien de but standard. Le gardien doit se placer entre la balle et le but, tout en restant à
-    l'intérieur de son demi-cercle. Si la balle entre dans son demi-cercle, le gardien tente d'aller en prendre
-    possession. Il s'agit d'une version simple, mais fonctionelle du gardien. Peut être améliorer.
-    méthodes:
-        exec(self) : Exécute une Action selon l'état courant
-    attributs:
-        game_state: L'état courant du jeu.
-        player_id : Identifiant du gardien de but
-        current_state : L'état courant de la tactique
-        next_state : L'état suivant de la tactique
-        status_flag : L'indicateur de progression de la tactique
-        is_yellow : un booléen indiquant si le gardien est dans l'équipe des jaunes, ce qui détermine quel but est
-        protégé. Les jaunes protègent le but de droite et les bleus, le but de gauche.
     """
     # TODO: À complexifier pour prendre en compte la position des jouers adverses et la vitesse de la balle.
 
@@ -52,7 +39,7 @@ class AssistantGoalkeeper(Tactic):
     def protect_goal(self):
         # FIXME : enlever ce hack de merde
         ball_position = self.game_state.get_ball_position()
-        if not isInsideGoalArea(ball_position, self.is_yellow):
+        if not isInsideGoalArea(ball_position):
             self.next_state = self.protect_goal
         else:
             if canGetBall(self.game_state, self.player_id, ball_position):
@@ -60,7 +47,8 @@ class AssistantGoalkeeper(Tactic):
             else:
                 self.next_state = self.go_behind_ball
         self.target = Pose(self.game_state.get_ball_position())
-        return ProtectGoal(self.game_state, self.player_id, self.is_yellow, p_minimum_distance=250)
+
+        return GoToPositionNoPathfinder(self.game_state, self.player_id)
 
     def go_behind_ball(self):
         ball_position = self.game_state.get_ball_position()
