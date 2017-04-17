kalman covariance fix

RULEngine/Util/tracking.py
@@ -30,12 +30,12 @@ class Kalman:
             self.H += [[0, 0, 0, 0, 1, 0] for i in range(ncameras)] # Orientation
             self.H = np.array(self.H)
             # Process covariance
-            values = np.array([10 ** (0), 10 ** (0), 10 ** (1), 10 ** (1), 10 ** (-2), 10 ** (0)])
+            values = np.array([10 ** (0), 10 ** (0), 10 ** (1), 10 ** (1), 10 ** (-2), 10 ** (-1)])
             self.Q = np.diag(values)
             # Observation covariance
-            values = [10 ** (0), 10 ** (0), 10 ** (-2)]
-            for i in range(ncameras-1):
-                values += values
+            values = [10 ** (0) for i in range(ncameras)]
+            values += [10 ** (0) for i in range(ncameras)]
+            values += [10 ** (-3) for i in range(ncameras)]
             self.R = np.diag(values)  # Pose * ncameras
             # Initial state covariance
             self.P = 10 ** (3) * np.eye(6)
@@ -69,9 +69,9 @@ class Kalman:
             values = np.array([10 ** (0), 10 ** (0), 10 ** (0), 10 ** (0), 10 ** (2),  10 ** (-1)])
             self.Q = np.diag(values)
             # Observation covariance
-            values = [10 ** (0), 10 ** (0), 10 ** (-3)]
-            for i in range(ncameras - 1):
-                values += values
+            values = [10 ** (0) for i in range(ncameras)]
+            values += [10 ** (0) for i in range(ncameras)]
+            values += [10 ** (-3) for i in range(ncameras)]
             self.R = np.diag(values)  # Pose * ncameras
             # Initial state covariance
             self.P = 10 ** (3) * np.eye(6)
@@ -102,9 +102,8 @@ class Kalman:
             values = np.array([10 ** (0), 10 ** (0), 10 ** (0), 10 ** (0)])
             self.Q = np.diag(values)
             # Observation covariance
-            values = [10 ** (0), 10 ** (0)]
-            for i in range(ncameras - 1):
-                values += values
+            values = [10 ** (0) for i in range(ncameras)]
+            values += [10 ** (0) for i in range(ncameras)]
             self.R = np.diag(values)  # Pose * ncameras
             # Initial state covariance
             self.P = 10 ** (3) * np.eye(4)