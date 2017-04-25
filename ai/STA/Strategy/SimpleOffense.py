c64a4a17df54657014a2a21c83bc476a86e6ce9f
Léonard Gagnon <leonard.gagnon@gmail.com>
2017-04-24 22:05:56 -0400

    Strategy SimpleOffense WIP

--- ai/STA/Strategy/SimpleOffense.py
+++ ai/STA/Strategy/SimpleOffense.py
@@ -1,12 +1,13 @@
 # Under MIT license, see LICENSE.txt
 from functools import partial
 
-from RULEngine.Command.command import Stop
 from RULEngine.Util.Pose import Pose
 from RULEngine.Util.Position import Position
 from RULEngine.Util.constant import PLAYER_PER_TEAM
 from ai.STA.Tactic.GoGetBall import GoGetBall
 from ai.STA.Tactic.GoalKeeper import GoalKeeper
+from ai.STA.Tactic.Stop import Stop
+from ai.STA.Tactic.capture import Capture
 from ai.STA.Tactic.go_kick import GoKick
 from ai.STA.Tactic.position_for_pass import PositionForPass
 from ai.STA.Tactic.tactic_constants import Flags
@@ -26,22 +27,37 @@ class SimpleOffense(Strategy):
         goal2 = Pose(Position(1636, 0))
         self.add_tactic(self.goalkeeper, GoalKeeper(self.game_state, self.goalkeeper))
 
-        receiver_pose = Pose(Position(-500, 200))
+        receiver_pose = Pose(Position(-500, -500))
         self.add_tactic(self.receiver, PositionForPass(self.game_state, self.receiver, receiver_pose))
         self.add_tactic(self.receiver, GoKick(self.game_state, self.receiver, goal1))
-        self.add_condition(self.receiver, 0, 1, partial(self.passing_robot_success, self.receiver))
-        self.add_condition(self.receiver, 1, 0, partial(self.receiver_kick_to_goal, self.receiver))
+        self.add_condition(self.receiver, 0, 1, partial(self.is_passing_robot_success, self.receiver))
+        self.add_condition(self.receiver, 1, 0, partial(self.has_receiver_kicked_to_goal, self.receiver))
 
+
+        self.add_tactic(self.passing_robot, Capture(self.game_state, self.passing_robot, receiver_pose))
         self.add_tactic(self.passing_robot, GoKick(self.game_state, self.passing_robot, receiver_pose))
-        self.add_condition(self.passing_robot, 0, 0, partial(self.receiver_kick_to_goal, self.passing_robot))
+        self.add_tactic(self.passing_robot, Stop(self.game_state, self.passing_robot))
+        self.add_condition(self.passing_robot, 0, 1, partial(self.is_receiver_ready, self.passing_robot))
+        self.add_condition(self.passing_robot, 1, 2, partial(self.is_passing_robot_success, self.passing_robot))
+        self.add_condition(self.passing_robot, 2, 0, partial(self.has_receiver_kicked_to_goal, self.passing_robot))
+        self.add_condition(self.passing_robot, 2, 1, partial(self.is_receiver_ready, self.passing_robot))
 
         for i in range(PLAYER_PER_TEAM):
             if not (i == self.goalkeeper or i == self.passing_robot or i == self.receiver):
                 self.add_tactic(i, Stop(self.game_state, i))
 
 
-    def passing_robot_success(self):
-        return self.graphs[self.passing_robot].get_current_tactic().status_flag == Flags.SUCCESS
+    def is_passing_robot_success(self, i):
+        return (self.game_state.get_ball_position().x < 0 and self.game_state.get_ball_position().y < 0)
 
-    def receiver_kick_to_goal(self):
-        return self.graphs[self.receiver].get_current_tactic().status_flag == Flags.SUCCESS
+    def has_receiver_kicked_to_goal(self, i):
+        if self.graphs[self.receiver].get_current_tactic_name() == 'GoKick':
+            return self.graphs[self.receiver].get_current_tactic().status_flag == Flags.SUCCESS
+        else:
+            return False
+
+    def is_receiver_ready(self, i):
+        if self.graphs[self.receiver].get_current_tactic_name() == 'PositionForPass':
+            return self.graphs[self.receiver].get_current_tactic().status_flag == Flags.SUCCESS
+        else:
+            return False
