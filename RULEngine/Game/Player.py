6a05ffd5d9e4a0ad31b6e4fba857f471665a4865
philippe lebel <philippe.lebel008@sympatico.ca>
2017-04-24 23:28:35 -0400

    try d'afficher le path

--- RULEngine/Game/Player.py
+++ RULEngine/Game/Player.py
@@ -32,7 +32,6 @@ class Player:
         ret = self.kf.filter(poses, self.cmd, delta)
         self.pose = Pose(Position(ret[0], ret[1]), ret[4])
         self.velocity = [ret[2], ret[3], ret[5]]
-        print(self.velocity)
 
     def set_command(self, cmd):
         self.cmd = [cmd.pose.position.x, cmd.pose.position.y, cmd.pose.orientation]
