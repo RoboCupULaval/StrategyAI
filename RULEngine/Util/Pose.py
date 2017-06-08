718698d9192cff26399061d46c2b09245c0aa09b
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2017-06-07 23:10:44 -0400

    The Pose and Position now work like a ndarray. Every numpy function can be use on them. They still work with legacy code. conv_2_np() should not be use on new code.

--- RULEngine/Util/Pose.py
+++ RULEngine/Util/Pose.py
@@ -3,40 +3,44 @@ from .Position import Position
 import numpy as np
 
 
-ORIENTATION_DELTA_TOLERANCE_MAGNITUDE = 1e4
+class Pose(np.ndarray):
 
+    def __new__(cls, position=Position(), orientation=0):
+        obj = np.asarray(np.append(position, orientation)).view(cls)
+        obj.position = position
+        obj.orientation = orientation
+        return obj
 
-class Pose(object):
-    """  Container of position and orientation """
-    def __init__(self, position=Position(), orientation=0.0):
-        assert(isinstance(position, Position)), 'position should be Position object.'
-        assert(isinstance(orientation, (int, float))), 'orientation should be int or float value.'
+    @property
+    def position(self):
+        return self[0:2].view(Position)
 
-        self.position = position
-        self.orientation = orientation
-        '''
-        if self.orientation >= m.pi:
-            self.orientation -= 2 * m.pi
-        elif self.orientation <= -m.pi:
-            self.orientation += 2*m.pi
-        '''
+    @position.setter
+    def position(self, position: Position):
+        self[0:2] = position[0:2]  # The height og the ball (z component) is discard in a pose
+
+    @property
+    def orientation(self):
+        return float(self[2])
+
+    @orientation.setter
+    def orientation(self, orientation):
+        self[2] = orientation
+
     def to_tuple(self):
-        """ Retourne la position de la pose sous forme d'un tuple."""
         return self.position.x, self.position.y
 
-    def conv_2_np(self):
-        return np.array([self.position.x, self.position.y, self.orientation])
+    def conv_2_np(self) -> np.ndarray:
+        return self
 
     def __str__(self):
-        return '[{}, theta={}]'.format(self.position, self.orientation)
+        return '[pos={}, theta={}]'.format(self.position, self.orientation)
+
     def __repr__(self):
         return self.__str__()
 
     def __eq__(self, other):
-        orientation_left_side = int(self.orientation * ORIENTATION_DELTA_TOLERANCE_MAGNITUDE)
-        orientation_right_side = int(other.orientation * ORIENTATION_DELTA_TOLERANCE_MAGNITUDE)
-        orientation_test = orientation_left_side == orientation_right_side
-        return self.position == other.position and orientation_test
+        return bool(self.position == other.position and np.isclose(self.orientation, other.orientation, atol=0.01))
 
     def __ne__(self, other):
         return not self.__eq__(other)
--- RULEngine/Util/Position.py
+++ RULEngine/Util/Position.py
@@ -1,105 +1,48 @@
 # Under MIT License, see LICENSE.txt
 
-import math
 import numpy as np
 
-POSITION_DELTA_TOLERANCE_MAGNITUDE = 0.01
 
+class Position(np.ndarray):
+    def __new__(cls, x=0, y=0, z=0):
+        obj = np.asarray(np.array([x, y])).view(cls)
+        obj.x = x
+        obj.y = y
+        obj.z = z
+        return obj
 
-class Position(object):
-    """ Vector with [x, y, z] """
-    def __init__(self, x=0., y=0., z=0., abs_tol=POSITION_DELTA_TOLERANCE_MAGNITUDE, delta_t=0.03):
-        # assert(isinstance(x, (int, float))), 'x should be int or float.'
-        # assert(isinstance(y, (int, float))), 'y should be int or float.'
-        # assert(isinstance(z, (int, float))), 'z should be int or float.'
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.z = getattr(obj, 'z', 0)
 
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
-        self.abs_tol = abs_tol
-        self.delta_t = delta_t
+    @property
+    def x(self):
+        return float(self[0])
 
-    def copy(self):
-        """
-        copy() -> Position
+    @x.setter
+    def x(self, x):
+        self[0] = x
 
-        Return copy of Position.
-        """
-        return Position(self.x, self.y, self.z)
+    @property
+    def y(self):
+        return float(self[1])
 
-    def get_delta_t(self):
-        return self.delta_t
+    @y.setter
+    def y(self, y):
+        self[1] = y
+
+    def __eq__(self, other):
+        return bool(np.all(np.isclose(self, other, atol=0.01)))
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def conv_2_np(self):
-        return np.array([self.x, self.y])
+        return self
 
     @staticmethod
     def from_np(array):
-        if array is Position:
-            return array
         return Position(array[0], array[1])
 
-    # *** OPERATORS ***
-    def __add__(self, other):
-        """ Return self + other """
-        if not isinstance(other, (Position, int, float)):
-            return NotImplemented
-        else:
-            new_x = self.x + (other.x if isinstance(other, Position) else other)
-            new_y = self.y + (other.y if isinstance(other, Position) else other)
-            return Position(new_x, new_y)
 
-    def __sub__(self, other):
-        """ Return self - other """
-        # if not isinstance(other, (Position, int, float)):
-        #     raise NotImplementedError
-        # else:
-        #     new_x = self.x - (other.x if isinstance(other, Position) else other)
-        #     new_y = self.y - (other.y if isinstance(other, Position) else other)
-        #     return Position(new_x, new_y)
-        new_x = self.x - other.x
-        new_y = self.y - other.y
-        return Position(new_x, new_y)
-
-    def __mul__(self, other):
-        """ Return self * other """
-        if not isinstance(other, (int, float)):
-            raise NotImplementedError
-        else:
-            new_x = self.x * other
-            new_y = self.y * other
-            return Position(new_x, new_y)
-
-    def __truediv__(self, other):
-        """ Return self / other """
-        if not isinstance(other, (int, float)):
-            raise NotImplementedError
-        else:
-            new_x = self.x / other
-            new_y = self.y / other
-            return Position(new_x, new_y)
-
-    def __eq__(self, other):
-        """
-            L'égalité est vérifié au niveau de l'unité.
-            La comparison de float exige toujours un seuil de tolérance.
-            Dans ce cas-ci, les décimales n'importent pas.
-            Position(0.5, 0) == Position(0, 0) -> True
-            (multiplication par la magnitude de tolérance, définie dans constant.py, puis conversion pour couper les décimales)
-        """
-        min_abs_tol = min(self.abs_tol, other.abs_tol)
-        return math.isclose(self.x, other.x, abs_tol=min_abs_tol) and math.isclose(self.y, other.y, abs_tol=min_abs_tol)
-
-    def __ne__(self, other):
-        """ Return self != other """
-        return not self.__eq__(other)
-
-    def __repr__(self):
-        """ Return str(self) """
-        return "(x={}, y={}, z={})".format(self.x, self.y, self.z)
-
-    def __str__(self):
-        return "(x={}, y={}, z={})".format(self.x, self.y, self.z)
-
-    def __hash__(self):
-        return hash(str(self))
