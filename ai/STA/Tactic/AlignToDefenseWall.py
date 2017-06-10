6f79203b443cf673086f7b40410dba39581237e3
philippe lebel <philippe.lebel008@sympatico.ca>
2017-06-10 02:33:35 -0400

    layout de la premiere tactique

--- ai/STA/Tactic/AlignToDefenseWall.py
+++ ai/STA/Tactic/AlignToDefenseWall.py
@@ -1,10 +1,13 @@
 # Under MIT licence, see LICENCE.txt
-
+from typing import List
 import numpy as np
+import time
 
+from RULEngine.Game.OurPlayer import OurPlayer
+from RULEngine.Util.Pose import Pose
 from RULEngine.Util.constant import BALL_RADIUS, ROBOT_RADIUS
 from ai.STA.Tactic.Tactic import Tactic
-
+from ai.states.game_state import GameState
 
 __author__ = 'RoboCupULaval'
 
@@ -14,6 +17,15 @@ DISTANCE_TO_KICK_SIM = ROBOT_RADIUS + BALL_RADIUS
 COMMAND_DELAY = 1.0
 
 
-class GoKick(Tactic):
-    def __init__(self):
-        self.player = None
+class AllignToDefenseWall(Tactic):
+    def __init__(self, game_state: GameState, player: OurPlayer, number_of_robots: int=0, robot_formation_number: int=0,
+                 args: List[str]=None):
+        Tactic.__init__(self, game_state, player, args=args)
+        self.current_state = self.go_allign
+        self.next_state = self.go_allign
+        self.last_time = time.time()
+        self.number_of_robots = number_of_robots
+        self.robot_formation_number = robot_formation_number
+
+    def go_allign(self):
+        #dodo time
