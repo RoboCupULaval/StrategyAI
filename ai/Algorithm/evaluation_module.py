a471ffa4fa83af298d426d4e0bb8a24f85dc0eab
Philippe Turgeon <philippe.turgeon.4@ulaval.ca>
2017-06-13 00:03:28 -0400

    COrrection bug de nan

--- ai/Algorithm/evaluation_module.py
+++ ai/Algorithm/evaluation_module.py
@@ -1,6 +1,7 @@
 # Under MIT License, see LICENSE.txt
 
 import numpy as np
+import math
 
 from RULEngine.Util import Position
 from RULEngine.Util.Pose import Pose
@@ -8,7 +9,7 @@ from RULEngine.Util.constant import PLAYER_PER_TEAM, TeamColor
 from RULEngine.Util.geometry import *
 from RULEngine.Util.team_color_service import TeamColorService
 from ai.states.game_state import GameState
-
+from typing import Union
 
 def player_with_ball(min_dist_from_ball):
     # Retourne le joueur qui possède la balle, NONE si balle libre
@@ -46,14 +47,14 @@ def is_ball_moving(min_speed=0.1):
 def is_ball_our_side():
     # Retourne TRUE si la balle est dans notre demi-terrain
     if GameState().const["FIELD_GOAL_YELLOW_X_LEFT"] > 0:
-        if TeamColorService().OUR_TEAM_COLOR is TeamColor.BLUE_TEAM: # BLUE_TEAM
+        if TeamColorService().OUR_TEAM_COLOR is TeamColor.BLUE: # BLUE
             return GameState().get_ball_position().x < 0
-        else: # YELLOW_TEAM
+        else: # YELLOW
             return GameState().get_ball_position().x > 0
     else:
-        if TeamColorService().OUR_TEAM_COLOR is TeamColor.BLUE_TEAM: # BLUE_TEAM
+        if TeamColorService().OUR_TEAM_COLOR is TeamColor.BLUE: # BLUE
             return GameState().get_ball_position().x > 0
-        else: # YELLOW_TEAM
+        else: # YELLOW
             return GameState().get_ball_position().x < 0
 
 
@@ -66,29 +67,28 @@ def best_passing_option(passing_player):
     # Retourne l'ID du player ou le but le mieux placé pour une passe, NONE si but est la meilleure possibilité
 
     score_min = float("inf")
-    if TeamColorService().OUR_TEAM_COLOR is TeamColor.YELLOW_TEAM :# YELLOW_TEAM
+    if TeamColorService().OUR_TEAM_COLOR is TeamColor.YELLOW :# YELLOW
         goal = Position(GameState().field.constant["FIELD_GOAL_BLUE_X_LEFT"], 0)
     else:
         goal = Position(GameState().field.constant["FIELD_GOAL_YELLOW_X_RIGHT"], 0)
 
     for i in GameState().my_team.available_players.values():
-        if i is not passing_player:
+        if i.id != passing_player.id:
             # Calcul du score pour passeur vers receveur
             score = line_of_sight_clearance(passing_player, i.pose.position)
 
             # Calcul du score pour receveur vers but
             score += line_of_sight_clearance(i, goal)
-            score = np.log(score)
+            score = (score)
             if score_min > score:
                 score_min = score
                 receiver_id = i
-            print('player position :', i.pose.position, score, i.id)
+            print(score, i.id)
 
-    score = np.log(line_of_sight_clearance(passing_player, goal))
+    score = (line_of_sight_clearance(passing_player, goal))
     if score_min > score:
         receiver_id = None
     print(score, 'but')
-
 
     return receiver_id
 
@@ -99,11 +99,11 @@ def line_of_sight_clearance(player, target: Position):
     score = np.linalg.norm(player.pose.position - target)
     for j in GameState().my_team.available_players.values():
         # Obstacle : les players friends
-        if not j.id == player.id:
+        if not (j.id == player.id or j.pose.position == target):
             score *= trajectory_score(player.pose.position, target, j.pose.position)
-    # for j in GameState().other_team.available_players.values():
-    #     # Obstacle : les players ennemis
-    #     score *= trajectory_score(player.pose.position, target, j.pose.position)
+    for j in GameState().other_team.available_players.values():
+        # Obstacle : les players ennemis
+        score *= trajectory_score(player.pose.position, target, j.pose.position)
     return score
 
 
@@ -116,7 +116,8 @@ def trajectory_score(pointA : Position, pointB: Position, obstacle: Position):
     if (normAC < 0) or (normAC > 1.1 * np.linalg.norm(AB)):
         return 1
     else:
-        return max(1, min(normAC / normOC, 100000))
+        return max(1, min(normAC / normOC, 15))
+
 
 
 def is_player_facing_target(player, target_position: Position, tolerated_angle: float) -> bool:
