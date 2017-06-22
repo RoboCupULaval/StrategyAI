2b3a006f1b1a4b7f21775671af5f4aa706bc2134
david2194 <david.cote.12@ulaval.ca>
2017-06-21 21:53:28 -0400

    Moved and fixed tests

--- tests/states/test_role_mapper.py
+++ tests/states/test_role_mapper.py
@@ -1,36 +1,36 @@
 from unittest import TestCase
-from ..Util.role import Role
-from .game_state import GameState
+from ai.Util.role import Role
+from ai.states.game_state import GameState
 
 class RoleMapperTests(TestCase):
     def test_givenNoMapping_whenMapById_thenMapsAllPlayers(self):
         state = GameState()
-        state.map_players_to_roles_by_id(basic_roles)
+        state.map_players_to_roles_by_player(basic_roles)
         self.assertDictEqual(state.get_role_mapping(), basic_roles)
 
     def test_givenBasicMapping_whenMapOtherwise_thenMapsPlayersProperly(self):
         state = GameState()
-        state.map_players_to_roles_by_id(basic_roles)
-        state.map_players_to_roles_by_id(inverted_roles_no_goal)
+        state.map_players_to_roles_by_player(basic_roles)
+        state.map_players_to_roles_by_player(inverted_roles_no_goal)
         self.assertDictEqual(state.get_role_mapping(), inverted_roles_no_goal)
 
     def test_givenBasicMapping_whenMapFewerRobots_thenRemovesUnasignedOnes(self):
         state = GameState()
-        state.map_players_to_roles_by_id(basic_roles)
-        state.map_players_to_roles_by_id(missing_middle)
+        state.map_players_to_roles_by_player(basic_roles)
+        state.map_players_to_roles_by_player(missing_middle)
         self.assertDictEqual(state.get_role_mapping(), missing_middle)
 
     def test_givenBasicMapping_whenMapMissingLockedRole_thenKeepsLockedRole(self):
         state = GameState()
-        state.map_players_to_roles_by_id(basic_roles)
-        state.map_players_to_roles_by_id(missing_required)
+        state.map_players_to_roles_by_player(basic_roles)
+        state.map_players_to_roles_by_player(missing_required)
         self.assertDictEqual(state.get_role_mapping(), missing_required_expected)
 
     def test_givenBasicMapping_whenRemapLockedRole_thenThrowsValueError(self):
         state = GameState()
-        state.map_players_to_roles_by_id(basic_roles)
+        state.map_players_to_roles_by_player(basic_roles)
         with self.assertRaises(ValueError):
-            state.map_players_to_roles_by_id(inverted_roles)
+            state.map_players_to_roles_by_player(inverted_roles)
 
 
 basic_roles = {
