3950219afbc63f32f33f98da6dc0d6eba0c47d1f
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2017-06-28 20:44:16 -0400

    Cleaned RotateAround

--- ai/STA/Action/rotate_around.py
+++ ai/STA/Action/rotate_around.py
@@ -3,20 +3,21 @@ import numpy as np
 
 from RULEngine.Game.OurPlayer import OurPlayer
 from RULEngine.Util.Pose import Pose
-from RULEngine.Util.Position import Position
 
 from ai.states.game_state import GameState
 from ai.STA.Action.Action import Action
 from ai.Util.ai_command import AICommand, AICommandType
 
+from typing import Union
+
 
 class RotateAround(Action):
-    def __init__(self, game_state: GameState, player: OurPlayer, target: Pose, rayon: [int, float]):
+    def __init__(self, game_state: GameState, player: OurPlayer, target: Pose, rayon: Union[int, float]):
         """
-            :param game_state: L'état courant du jeu.
-            :param player: Instance du joueur qui se déplace
-            :param target: Pose du centre de rotation
-            :param rayon: Distance entre le centre du robot et le centre de rotation
+            :param game_state: current game state
+            :param player: Instance of the player (OurPlayer)
+            :param target: Position of the center of rotation
+            :param rayon: Radius of the rotation around target position
         """
         Action.__init__(self, game_state, player)
         self.target = target
@@ -24,21 +25,18 @@ class RotateAround(Action):
         self.rayon = rayon
 
     def generate_destination(self):
-        player = self.player.pose.position.conv_2_np()
-        target = self.target.position.conv_2_np()
-        player_to_target_orientation = np.arctan2(target[1] - player[1], target[0] - player[0])
+        player = self.player.pose.position
+        target = self.target.position
+        player_to_target = target-player
+        player_to_target_orientation = player_to_target.angle()
         target_orientation = self.target.orientation
         delta_theta = player_to_target_orientation - target_orientation
         delta_theta = min(abs(delta_theta), np.pi/6) * np.sign(delta_theta)
-        rotation_matrix = np.array([[np.cos(delta_theta), np.sin(delta_theta)], [-np.sin(delta_theta), np.cos(delta_theta)]])
-        player_to_ball_rot = np.dot(rotation_matrix, player - target)
-        translation = player_to_ball_rot / np.linalg.norm(player_to_ball_rot) * self.rayon
+        player_to_ball_rot = -player_to_target.rotate(delta_theta)
+        translation = player_to_ball_rot.normalized() * self.rayon
         destination = translation + target
         orientation = target_orientation
-        return Pose(Position.from_np(destination), orientation)
+        return Pose(destination, orientation)
 
     def exec(self):
-        """
-        Exécute le déplacement
-        """
         return AICommand(self.player, AICommandType.MOVE, **{"pose_goal": self.generate_destination()})
--- ai/STA/Tactic/RotateAroundPosition.py
+++ ai/STA/Tactic/RotateAroundPosition.py
@@ -16,16 +16,9 @@ class RotateAroundPosition(Tactic):
         super().__init__(game_state, player, target, args)
 
     def exec(self):
-        if self.check_success():
-            self.status_flag = Flags.SUCCESS
-        else:
-            self.status_flag = Flags.WIP
         ball = self.game_state.get_ball_position().conv_2_np()
         target = self.target.position.conv_2_np()
         ball_to_target = target - ball
         orientation = np.arctan2(ball_to_target[1], ball_to_target[0])
         next_action = RotateAround(self.game_state, self.player, Pose(Position.from_np(ball), orientation), 90)
         return next_action.exec()
-
-    def check_success(self):
-        return False
