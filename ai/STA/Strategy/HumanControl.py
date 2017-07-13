c1c6b02ff9efcd2ec87ba31cfcc68bdd676bee6f
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2017-07-13 10:10:14 -0400

    player 0 can now stop in humancontrol, remove prints

--- ai/STA/Strategy/HumanControl.py
+++ ai/STA/Strategy/HumanControl.py
@@ -21,6 +21,6 @@ class HumanControl(Strategy):
         assert isinstance(robot_id, int)
 
         r = self.game_state.get_role_by_player_id(robot_id)
-        if r:
+        if r is not None:
             self.roles_graph[r].remove_node(0)
             self.add_tactic(r, tactic)
