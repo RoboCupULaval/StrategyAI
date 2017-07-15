75632e699b6b83eba3d87506b2fed2de45f695bc
Léonard Gagnon <leonard.gagnon@gmail.com>
2017-07-15 14:24:30 -0400

    Fix StayAway strat for pull request

--- ai/STA/Strategy/stay_away.py
+++ ai/STA/Strategy/stay_away.py
@@ -8,10 +8,7 @@ class StayAway(Strategy):
     def __init__(self, p_game_state):
         super().__init__(p_game_state)
 
-        roles_to_consider = [Role.FIRST_ATTACK, Role.SECOND_ATTACK, Role.MIDDLE,
-                             Role.FIRST_DEFENCE, Role.SECOND_DEFENCE, Role.GOALKEEPER]
-        role_by_robots = [(i, self.game_state.get_player_by_role(i)) for i in roles_to_consider]
-
-        for index, player in role_by_robots:
-            if player:
-                self.add_tactic(index, StayAwayFromBall(self.game_state, player))
+        for r in Role:
+            p = self.game_state.get_player_by_role(r)
+            if p:
+                self.add_tactic(r, StayAwayFromBall(self.game_state, p))
