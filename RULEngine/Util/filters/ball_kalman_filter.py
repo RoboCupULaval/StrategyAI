Fixed kalman covariance

RULEngine/Util/filters/ball_kalman_filter.py
@@ -1,6 +1,6 @@
 import numpy as np
 from RULEngine.Util.filters.kalman_filter import KalmanFilter
-
+from math import fabs
 
 class BallFilter(KalmanFilter):
 
@@ -28,7 +28,12 @@ class BallFilter(KalmanFilter):
         return 10 ** 6 * np.eye(self.state_number)
 
     def process_covariance(self):
-        return np.diag([.1, .1, .1, .1])
+        return np.diag([.5, 5000, .5, 5000])
 
     def observation_covariance(self):
-        return 1 * np.eye(self.observable_state)
+        if fabs(self.x[0]) < 30 or fabs(self.x[2]) < 30:
+            R = np.diag([50, 50])
+        else:
+            R = np.diag([10, 10])
+        return R
+

RULEngine/Util/filters/kalman_filter.py
@@ -16,11 +16,12 @@ class KalmanFilter:
         self.state_number = int(np.size(self.transition_model(), 0))
         self.observable_state = int(np.size(self.observation_model(), 0))
 
+        self.x = np.zeros(self.state_number)
+
         self.R = self.observation_covariance()
         self.Q = self.process_covariance()
         self.P = self.initial_state_covariance()
 
-        self.x = np.zeros(self.state_number)
 
     @abstractmethod
     def transition_model(self):

RULEngine/Util/filters/robot_kalman_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from RULEngine.Util.filters.kalman_filter import KalmanFilter
+from math import fabs
 
 
 class RobotFilter(KalmanFilter):
@@ -43,10 +44,14 @@ class RobotFilter(KalmanFilter):
         return 10 ** 6 * np.eye(self.state_number)
 
     def process_covariance(self):
-        return np.diag([.1, .1, .1, .1, 0.01, 0.01])
+        return np.diag([.000005, .05, 0.000005, .05, 0.0001, 1.0])
 
     def observation_covariance(self):
-        return np.diag([1, 1, 0.01])
+        if fabs(self.x[0]) < 30 or fabs(self.x[2]) < 30:
+            R = np.diag([50, 50, 0.00005])
+        else:
+            R = np.diag([10, 10, 0.00005])
+        return R
 
     def update(self, observation, t_capture):
         error = observation - self.observation_model() @ self.x

RULEngine/Util/multiballservice.py
@@ -21,18 +21,10 @@ class MultiBallService(list):
         
     def update(self, obs: np.array, timestamp: float) -> None:
         self._current_timestamp = timestamp
-        if self.max_ball > 1:
-            closest_ball = self.find_closest_ball_to_observation(obs)
-        else:
-            closest_ball = self[0]
+        if self.filter_ball_observation(obs) or not self[0].is_active:
+            self[0].update(obs, self._current_timestamp)
 
-        if closest_ball is not None:
-            closest_ball.update(obs, self._current_timestamp)
-        else:
-            self.logger.info('New ball detected')
-            inactive_balls = [ball for ball in self if not ball.is_active]
-            if inactive_balls:
-                inactive_balls[0].update(obs, self._current_timestamp)
+        self.remove_undetected()
 
     def predict(self) -> None:
         for ball in self:
@@ -43,10 +35,18 @@ class MultiBallService(list):
                             if ball.is_active and
                             self._current_timestamp - ball.last_t_capture > MultiBallService.MAX_UNDETECTED_DELAY]
 
-        map(lambda ball: ball.reset(), undetected_balls)
+        for ball in undetected_balls:
+            ball.reset()
+            self.logger.info('Deactivating ball')
 
-        if undetected_balls:
-            self.logger.info('Deactivating {} ball(s)'.format(len(undetected_balls)))
+    def filter_ball_observation(self, obs: np.ndarray) -> bool:
+        position_differences = self.compute_distances_ball_to_observation(obs)
+
+        is_valid = False
+        if position_differences is not None and np.min(position_differences) < self.BALL_SEPARATION_THRESHOLD:
+            is_valid = True
+
+        return is_valid
 
     def find_closest_ball_to_observation(self, obs: np.ndarray) -> BallFilter:
         position_differences = self.compute_distances_ball_to_observation(obs)