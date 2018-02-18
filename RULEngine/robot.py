70eea357c7b295e3caed1d8613e1d63ad34ddd28
philippe <philippe.lebel008@sympatico.ca>
2018-02-17 22:13:27 -0500

    re-implement les predictions, avec ca les robots semblent aller mieux dans grsim. Reste a savoir pk mon progfil trapezoidal semble avoir un 'lag', peut etre c'est a cause qu'on envoie 4 commande par tour de boucle, un genre de buffer se rempli?

--- RULEngine/robot.py
+++ RULEngine/robot.py
@@ -1,8 +1,8 @@
 from Util import Pose
 from Util.path import Path
 
-MAX_LINEAR_SPEED = 400000  # mm/s
-MAX_LINEAR_ACCELERATION = 400000  # mm/s^2
+MAX_LINEAR_SPEED = 2000  # mm/s
+MAX_LINEAR_ACCELERATION = 2000  # mm/s^2
 MAX_ANGULAR_SPEED = 1  # rad/s
 MAX_ANGULAR_ACCELERATION = 1  # rad/s^2
 
