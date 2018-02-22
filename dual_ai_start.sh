2d0d3db7f8f065b918db5463a4cf9ee9c9ddda8a
philippe <philippe.lebel008@sympatico.ca>
2018-02-21 19:26:33 -0500

    removed file

--- dual_ai_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#start ai blue
-python main.py config/sim.cfg | python main.py config/sim_yellow.cfg | python ../UI-Debug/main.py config/field/sim.cfg blue | python ../UI-Debug/main.py config/field/sim.cfg yellow
