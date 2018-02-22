fee3734123519320d714d4fcec1e7b99662c854c
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-02-22 18:36:25 -0500

    Test for decimation in SerialCommandSender

--- RULEngine/Communication/sender/serial_command_sender.py
+++ RULEngine/Communication/sender/serial_command_sender.py
@@ -3,9 +3,14 @@
 from pyhermes import McuCommunicator
 
 from RULEngine.Communication.sender.sender_base_class import SenderBaseClass
+import time
 
 
 class SerialCommandSender(SenderBaseClass):
+
+    buffer = {}
+    last_sent_time = 0.0
+    send_timeout = 0.050
 
     def connect(self, connection_info):
         return McuCommunicator(timeout=0.1)
@@ -13,8 +18,15 @@ class SerialCommandSender(SenderBaseClass):
     def send_packet(self):
         packets_frame = self.queue.get()
         for packet in packets_frame.packet:
-            self.connection.sendSpeed(packet.robot_id,
-                                      packet.command['x']/1000,
-                                      packet.command['y']/1000,
-                                      packet.command['orientation'])
+            SerialCommandSender.buffer[packet.robot_id] = packet.command
+
+        dt = time.time() - SerialCommandSender.last_sent_time
+        if dt > SerialCommandSender.send_timeout:
+            self.send_buffer()
+
+    def send_buffer(self):
+        for robot_id, cmd in SerialCommandSender.buffer.items():
+            self.connection.sendSpeed(robot_id, cmd['x'] / 1000, cmd['y'] / 1000, cmd['orientation'])
+
+        SerialCommandSender.buffer.clear()
 
