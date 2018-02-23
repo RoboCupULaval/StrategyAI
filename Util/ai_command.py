8ce415e6787c9c751654f0fcc65d2dcc00596d06
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-02-23 12:08:25 -0500

    made cmd builder attributes protected

--- Util/ai_command.py
+++ Util/ai_command.py
@@ -19,47 +19,47 @@ class CmdBuilder:
     def __init__(self):
         # Does are the defaults values of AICommand
         # A robot should stay idle with those value
-        self.target = None
-        self.kick_type = None
-        self.kick_force = 0
-        self.charge_kick = False
-        self.dribbler_active = False
-        self.cruise_speed = 0
-        self.end_speed = 0
-        self.ball_collision = True
-        self.pathfinder_on = True
+        self._target = None
+        self._kick_type = None
+        self._kick_force = 0
+        self._charge_kick = False
+        self._dribbler_active = False
+        self._cruise_speed = 0
+        self._end_speed = 0
+        self._ball_collision = True
+        self._pathfinder_on = True
 
     def withMoveTo(self, target, cruise_speed=1, end_speed=0, ball_collision=True):
-        self.target = target
-        self.cruise_speed = cruise_speed
-        self.end_speed = end_speed
-        self.ball_collision = ball_collision
-        self.pathfinder_on = True
+        self._target = target
+        self._cruise_speed = cruise_speed
+        self._end_speed = end_speed
+        self._ball_collision = ball_collision
+        self._pathfinder_on = True
         return self
 
     def withKick(self, kick_force=1):
-        self.kick_force = kick_force
-        self.kick_type = 1  # For the moment we only have one type
+        self._kick_force = kick_force
+        self._kick_type = 1  # For the moment we only have one type
         return self
 
     def withForceDribbler(self):
-        self.dribbler_active = True
+        self._dribbler_active = True
         return self
 
     def withChargeKicker(self):
-        self.charge_kick = True
+        self._charge_kick = True
         return self
 
     def build(self):
-        return AICommand(self.target,
-                         self.kick_type,
-                         self.kick_force,
-                         self.charge_kick,
-                         self.dribbler_active,
-                         self.cruise_speed,
-                         self.end_speed,
-                         self.ball_collision,
-                         self.pathfinder_on)
+        return AICommand(self._target,
+                         self._kick_type,
+                         self._kick_force,
+                         self._charge_kick,
+                         self._dribbler_active,
+                         self._cruise_speed,
+                         self._end_speed,
+                         self._ball_collision,
+                         self._pathfinder_on)
 
 
 Idle = CmdBuilder().build()
