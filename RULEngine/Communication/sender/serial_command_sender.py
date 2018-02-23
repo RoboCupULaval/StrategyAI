bbca9f270cca0857f5b45862c52f79579a426695
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-02-23 12:50:12 -0500

    added lock and scheduler. Might not work.

--- RULEngine/Communication/sender/serial_command_sender.py
+++ RULEngine/Communication/sender/serial_command_sender.py
@@ -5,29 +5,48 @@ from pyhermes import McuCommunicator
 from RULEngine.Communication.sender.sender_base_class import SenderBaseClass
 import time
 
+from threading import RLock, Thread
+import sched
+
 
 class SerialCommandSender(SenderBaseClass):
 
     buffer = {}
-    last_sent_time = 0.0
-    send_timeout = 0.050
+    send_delay = 0.050
+    send_lock = RLock()
+    send_thread = None
 
     def connect(self, connection_info):
         return McuCommunicator(timeout=0.1)
 
+    def start(self):
+        s = sched.scheduler(time.time, time.sleep)
+        s.enter(SerialCommandSender.send_delay, 1, self.send_buffer, argument=(s,))
+        SerialCommandSender.send_thread = Thread(target=s.run())
+        SerialCommandSender.send_thread.start()
+        super().start()
+
     def send_packet(self):
         packets_frame = self.queue.get()
+
+        SerialCommandSender.send_lock.acquire()
+
         for packet in packets_frame.packet:
             SerialCommandSender.buffer[packet.robot_id] = packet.command
 
-        dt = time.time() - SerialCommandSender.last_sent_time
-        if dt > SerialCommandSender.send_timeout:
-            self.send_buffer()
+        SerialCommandSender.send_lock.release()
 
-    def send_buffer(self):
+    def send_buffer(self, s):
+
+        s.enter(SerialCommandSender.send_delay, 1, self.send_buffer, argument=(s,))
+
+        SerialCommandSender.send_lock.acquire()
+
         for robot_id, cmd in SerialCommandSender.buffer.items():
-            if not (cmd['x'] == cmd['y'] == 0):
+            if any(cmd.values()):
                 self.connection.sendSpeed(robot_id, cmd['x'] / 1000, cmd['y'] / 1000, cmd['orientation'])
 
         SerialCommandSender.buffer.clear()
+
+        SerialCommandSender.send_lock.release()
 
