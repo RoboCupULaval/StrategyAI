b7cb8c5d681e7c1dd68c66657c098b6377888071
Frederic St-Pierre <frederic.st-pierre.7@ulaval.ca>
2018-03-14 00:41:51 -0400

    fixed most Debug linting issues. transformed the single-use DebugCommand class into a debug_command function

--- Debug/debug_command_factory.py
+++ Debug/debug_command_factory.py
@@ -20,6 +20,7 @@ class Color(object):
     def repr(self):
         return self.r, self.g, self.b
 
+
 YELLOW = Color(181, 137, 0)
 ORANGE = Color(203, 75, 22)
 RED = Color(220, 50, 47)
@@ -33,17 +34,14 @@ LIGHT_GREEN = Color(0, 255, 0)
 DEFAULT_DEBUG_TIMEOUT = 1.0
 
 
-class DebugCommand:
-
-    def __new__(cls, data_type, data, link=None, version='1.0'):
-        command = dict()
-        command['name'] = 'StrategyAI'
-        command['version'] = version
-        command['type'] = data_type
-        command['link'] = link
-        command['data'] = data
-
-        return command
+def debug_command(data_type, data, link=None, version='1.0'):
+    command = dict()
+    command['name'] = 'StrategyAI'
+    command['version'] = version
+    command['type'] = data_type
+    command['link'] = link
+    command['data'] = data
+    return command
 
 
 class DebugCommandFactory:
@@ -55,29 +53,29 @@ class DebugCommandFactory:
         if not isinstance(message, str):
             raise TypeError('Message need to be an string. Type {} given'.format(type(message)))
 
-        return DebugCommand(2, {'level': level, 'message': message})
+        return debug_command(2, {'level': level, 'message': message})
 
     @staticmethod
     def books(strategy_book: Dict, strategy_default: Dict, tactic_book: Dict, tactic_default: Dict, action: List):
-        return DebugCommand(1001, {'strategy': strategy_book,
-                                     'strategy_default': strategy_default,
-                                     'tactic': tactic_book,
-                                     'tactic_default': tactic_default,
-                                     'action': action})
+        return debug_command(1001, {'strategy': strategy_book,
+                                    'strategy_default': strategy_default,
+                                    'tactic': tactic_book,
+                                    'tactic_default': tactic_default,
+                                    'action': action})
 
     @staticmethod
     def robot_strategic_state(player: Player, tactic: str, action: str, target: Tuple[int, int]):
         team_color = str(player.team.team_color)
         player_info = {player.id: {'tactic': tactic, 'action': action, 'target': target}}
         team_info = {team_color: player_info}
-        return DebugCommand(1002, team_info)
+        return debug_command(1002, team_info)
 
     @staticmethod
     def auto_play_info(referee_info: str, referee_team_info: Dict, auto_play_info: Dict, auto_flag: bool):
-        return DebugCommand(1005, {'referee': referee_info,
-                                     'referee_team': referee_team_info,
-                                     'auto_play': auto_play_info,
-                                     'auto_flag': auto_flag})
+        return debug_command(1005, {'referee': referee_info,
+                                    'referee_team': referee_team_info,
+                                    'auto_play': auto_play_info,
+                                    'auto_flag': auto_flag})
 
     @staticmethod
     def game_state(blue: List[Dict], yellow: List[Dict], balls: List[Dict]):
@@ -128,27 +126,24 @@ class DebugCommandFactory:
     def ball(position: Position, color=ORANGE, timeout=0.05):
         return [DebugCommandFactory.circle(position, 150, color=color, timeout=timeout)]
 
-
     @staticmethod
     def line(start: Position, end: Position, color=MAGENTA, timeout=DEFAULT_DEBUG_TIMEOUT):
-        return DebugCommand(3001, {'start': start.to_tuple(),
-                                   'end': end.to_tuple(),
-                                   'color': color.repr(),
-                                   'timeout': timeout})
-
+        return debug_command(3001, {'start': start.to_tuple(),
+                                    'end': end.to_tuple(),
+                                    'color': color.repr(),
+                                    'timeout': timeout})
 
     @staticmethod
     def circle(center: Position, radius, color=LIGHT_GREEN, is_fill=True, timeout=DEFAULT_DEBUG_TIMEOUT):
-        return DebugCommand(3003, {'center': center.to_tuple(),
-                                   'radius': radius,
-                                   'color': color.repr(),
-                                   'is_fill': is_fill,
-                                   'timeout': timeout})
-
+        return debug_command(3003, {'center': center.to_tuple(),
+                                    'radius': radius,
+                                    'color': color.repr(),
+                                    'is_fill': is_fill,
+                                    'timeout': timeout})
 
     @staticmethod
     def multiple_points(points: List[Position], color=VIOLET, width=5, link=None, timeout=DEFAULT_DEBUG_TIMEOUT):
-        return DebugCommand(3005, {'points': [point.to_tuple() for point in points],
-                                   'color': color.repr(),
-                                   'width': width,
-                                   'timeout': timeout}, link=link)
+        return debug_command(3005, {'points': [point.to_tuple() for point in points],
+                                    'color': color.repr(),
+                                    'width': width,
+                                    'timeout': timeout}, link=link)
