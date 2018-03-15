69aa548f3424e962d36eb314e097b04e5d3ae811
Frederic St-Pierre <frederic.st-pierre.7@ulaval.ca>
2018-03-15 09:33:44 -0400

    added callback for missed delay (zero-sleep/deadline), added 2 unit tests for it

--- Util/timing.py
+++ Util/timing.py
@@ -1,10 +1,14 @@
 import time
+import typing
 
-def create_fps_timer(desired_fps: float):
+def create_fps_timer(desired_fps: float, on_miss_callback: typing.Callable = None):
     ''' Returns a timing function that sleeps to allow constant time between iterations.
     The precision of the time between iterations is up to the host operating system.
     ON AVERAGE, it should be around the desired_fps value.
 
+    The callback is a function reference that accepts one parameter, which is the time
+    that lateness delay in seconds. This function is only called when the desired_fps
+    cannot be respected (in other words, the function would need to go back in time).
     '''
     assert desired_fps > 0, 'the desired_fps must be a strictly positive value.'
     timer_period = 1/desired_fps
@@ -21,6 +25,8 @@ def create_fps_timer(desired_fps: float):
 
         if time_to_sleep > 0:
             time.sleep(time_to_sleep)
+        elif on_miss_callback is not None:
+            on_miss_callback(-time_to_sleep)
 
         # Bookkeeping nonlocal variable for the next sleep
         next_time += timer_period
--- tests/Util/test_timing.py
+++ tests/Util/test_timing.py
@@ -5,28 +5,54 @@ from Util import timing
 
 def test_near_full_load_fps_timer():
     # example preliminary test
-    START = time.time()
-    TIME_TOTAL = 3
-    FPS = 10
-    NUM_ITERATIONS = TIME_TOTAL*FPS
-    sleep = timing.create_fps_timer(10)
+    start = time.time()
+    time_total = 2
+    fps = 10
+    num_iterations = time_total*fps
+    sleep = timing.create_fps_timer(fps)
     sleep()  # to initialize it
-    for i in range(NUM_ITERATIONS-1):  # -1 because of the first sleep
-        time.sleep(1/(FPS+0.05))  # simulate load very close to full load
+    for _ in range(num_iterations-1):  # -1 because of the first sleep
+        time.sleep(1/(fps+0.05))  # simulate load very close to full load
         sleep()
-    end_time = time.time()-START
-    assert end_time == pytest.approx(TIME_TOTAL, 0.1)
+    end_time = time.time()-start
+    assert end_time == pytest.approx(time_total, 0.1)
 
 
 def test_no_load_fps_timer():
     # example preliminary test
-    START = time.time()
-    TIME_TOTAL = 3
-    FPS = 10
-    NUM_ITERATIONS = TIME_TOTAL*FPS
-    sleep = timing.create_fps_timer(10)
+    start = time.time()
+    time_total = 2
+    fps = 10
+    num_iterations = time_total*fps
+    sleep = timing.create_fps_timer(fps)
     sleep()  # to initialize it
-    for i in range(NUM_ITERATIONS-1):  # -1 because of the first sleep
+    for _ in range(num_iterations-1):  # -1 because of the first sleep
         sleep()
-    end_time = time.time()-START
-    assert end_time == pytest.approx(TIME_TOTAL, 0.1)
+    end_time = time.time()-start
+    assert end_time == pytest.approx(time_total, 0.1)
+
+def test_callback_called_fps_timer():
+    fps = 10
+    callback_called = False
+
+    def mock_callback(time_var):  # pylint: disable=unused-argument
+        nonlocal callback_called
+        callback_called = True
+    sleep = timing.create_fps_timer(fps, on_miss_callback=mock_callback)
+    sleep()
+    time.sleep(0.2)
+    sleep()
+    assert callback_called is True
+
+def test_callback_delay_fps_timer():
+    fps = 10
+    callback_time = None
+
+    def mock_callback(time_var):
+        nonlocal callback_time
+        callback_time = time_var
+    sleep = timing.create_fps_timer(fps, on_miss_callback=mock_callback)
+    sleep()
+    time.sleep(0.2)
+    sleep()
+    assert callback_time >= 0
