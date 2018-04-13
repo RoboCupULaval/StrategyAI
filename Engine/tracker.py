ab935127433c6393bdd1dd698321218a1e1c593c
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-04-13 11:09:55 -0400

    modify info to debug

--- Engine/tracker.py
+++ Engine/tracker.py
@@ -99,14 +99,14 @@ class Tracker:
         for robot in active_robots:
             if self._current_timestamp - robot.last_capture_time > Tracker.MAX_UNDETECTED_DELAY:
                 robot.reset()
-                self.logger.info('Robot %d was undetected for more than %d seconds.',
+                self.logger.debug('Robot %d was undetected for more than %d seconds.',
                                  robot.id,
                                  Tracker.MAX_UNDETECTED_DELAY)
 
         if self._ball.is_active:
             if self._current_timestamp - self._ball.last_capture_time > Tracker.MAX_UNDETECTED_DELAY:
                 self._ball.reset()
-                self.logger.info('The ball was undetected for more than %d seconds.',
+                self.logger.debug('The ball was undetected for more than %d seconds.',
                                  Tracker.MAX_UNDETECTED_DELAY)
 
     @staticmethod
