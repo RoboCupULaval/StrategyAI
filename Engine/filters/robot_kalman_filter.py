ac1b1e8faaeb08e05346c3eb5d8187f3b377e8b9
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-04-13 11:07:58 -0400

    log new robot and undetected one.

--- Engine/filters/robot_kalman_filter.py
+++ Engine/filters/robot_kalman_filter.py
@@ -6,6 +6,10 @@ from Engine.filters.kalman_filter import KalmanFilter
 
 class RobotFilter(KalmanFilter):
 
+    def __init__(self, robot_id):
+        self._id = robot_id
+        super().__init__()
+
     @property
     def pose(self):
         if self.is_active:
@@ -20,6 +24,10 @@ class RobotFilter(KalmanFilter):
     def orientation(self):
         if self.is_active:
             return self.x[4]
+
+    @property
+    def id(self):
+        return self._id
 
     def transition_model(self):
         dt = self._dt
