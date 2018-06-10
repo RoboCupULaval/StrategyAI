48d505ee521cf2b64cea6bc059b703f1413ab17f
Christian <barch2712@hotmail.com>
2018-06-10 15:32:07 -0400

    switch to waypoints

--- ai/STA/Tactic/stress_test_robot.py
+++ ai/STA/Tactic/stress_test_robot.py
@@ -5,12 +5,10 @@ from Util.ai_command import CmdBuilder
 from Util.constant import ROBOT_RADIUS
 from ai.GameDomainObjects import Player
 from ai.STA.Tactic.tactic import Tactic
+from ai.executors.pathfinder_module import WayPoint
 from ai.states.game_state import GameState
 
-COORD_X = 3000
-COORD_Y = 2000
 VALID_DISTANCE = ROBOT_RADIUS * 0.5
-CLOCKWISE = 1   # -1 for counter-clockwise
 DEFAULT_SPEED = 2
 
 
@@ -24,16 +22,19 @@ class StressTestRobot(Tactic):
         self.iteration = 0
         self.x_sign = 1
         self.y_sign = 1
+        self.coord_x = game_state.field.field_length/3
+        self.coord_y = game_state.field.field_width/3
+        self.points = [WayPoint(Position(self.coord_x, self.coord_y)),
+                       WayPoint(Position(-self.coord_x, self.coord_y)),
+                       WayPoint(Position(-self.coord_x, -self.coord_y)),
+                       WayPoint(Position(self.coord_x, -self.coord_y))]
 
     def next_corner(self):
-        position = Position(COORD_X * self.x_sign, COORD_Y * self.y_sign)
-        orientation = (position - self.player.position).angle
-        if (position - self.player.position).norm < VALID_DISTANCE:
-            self._switch_signs()
-        return CmdBuilder().addMoveTo(Pose(position, orientation), cruise_speed=self.speed).build()
+        orientation = (self.points[0].position - self.player.position).angle
 
-    def _switch_signs(self):
-        if self.x_sign * self.y_sign == CLOCKWISE:
-            self.x_sign *= -1
-        else:
-            self.y_sign *= -1
+        if (self.player.position-self.points[0].position).norm < 500:
+            first_ele = self.points.pop(-1)
+            self.points = [first_ele] + self.points
+        return CmdBuilder().addMoveTo(Pose(self.points[0].position, orientation),
+                                      way_points=self.points,
+                                      cruise_speed=self.speed).build()
