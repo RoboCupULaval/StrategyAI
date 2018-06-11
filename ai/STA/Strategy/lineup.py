6f86276c1ff29c2a0ed070a984b3e2788d03dec0
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-06-11 12:37:32 -0400

    fix lineup

--- ai/STA/Strategy/lineup.py
+++ ai/STA/Strategy/lineup.py
@@ -1,48 +1,33 @@
 # Under MIT license, see LICENSE.txt
-from functools import partial
+
 
 from Util import Position, Pose
 from Util.constant import ROBOT_RADIUS
 from Util.role import Role
 from Util.role_mapping_rule import keep_prev_mapping_otherwise_random
+from ai.STA.Strategy.team_go_to_position import TeamGoToPosition
 from ai.states.game_state import GameState
-from ai.STA.Strategy.strategy import Strategy
-from ai.STA.Tactic.go_to_position_pathfinder import GoToPositionPathfinder
 
 
-class LineUp(Strategy):
+class LineUp(TeamGoToPosition):
     def __init__(self, game_state: GameState):
         super().__init__(game_state)
-        self.target_position = Position(0, 0)
-        roles_to_consider = [Role.FIRST_ATTACK, Role.SECOND_ATTACK, Role.MIDDLE,
-                             Role.FIRST_DEFENCE, Role.SECOND_DEFENCE, Role.GOALKEEPER]
 
-        # self.robots_in_lineup = closest_players_to_point(self.target_position, True)
+        self.target_position = Position(self.game_state.field.our_goal_area.left - 3*ROBOT_RADIUS, 0)
 
-        role_by_robots = [(i, self.game_state.get_player_by_role(i)) for i in roles_to_consider]
+        number_of_player = len(self.game_state.our_team.available_players)
 
-        self.robots = [player for player in role_by_robots if player is not None]
-        i = 1
-        for role, player in role_by_robots:
+        role_to_positions = {}
+        for i, role in enumerate(Role.as_list()):
+            position_offset = Position(0, (i-(number_of_player-1)/2) * ROBOT_RADIUS * 4)
+            role_to_positions[role] = Pose(self.target_position + position_offset)
 
-            destination_orientation = 0
-            position_offset = Position(i * ROBOT_RADIUS * 3, 0)
-            positions_in_formations = self.target_position + position_offset
-            i += 1
-            self.create_node(role, GoToPositionPathfinder(self.game_state, player,
-                                                          Pose(positions_in_formations,
-                                                               destination_orientation)))
+        self.assign_tactics(role_to_positions)
 
     @classmethod
     def required_roles(cls):
-        return {r: keep_prev_mapping_otherwise_random for r in [Role.GOALKEEPER]
-                }
+        return {}
 
     @classmethod
     def optional_roles(cls):
-        return {r: keep_prev_mapping_otherwise_random for r in [Role.FIRST_DEFENCE,
-                                                                Role.SECOND_DEFENCE,
-                                                                Role.FIRST_ATTACK,
-                                                                Role.MIDDLE,
-                                                                Role.SECOND_ATTACK]
-                }
+        return {r: keep_prev_mapping_otherwise_random for r in Role.as_list()}
