5e17fbffc31603690db8d9338d8f7ffeec60d2ca
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-06-13 15:35:53 -0400

    revert some more files

--- Engine/robot.py
+++ Engine/robot.py
@@ -4,8 +4,8 @@ from Util import Pose, Position, Path
 from Util.geometry import wrap_to_pi
 
 
-MAX_LINEAR_SPEED = 4000  # mm/s
-MAX_LINEAR_ACCELERATION = 4000  # mm/s^2
+MAX_LINEAR_SPEED = 2000  # mm/s
+MAX_LINEAR_ACCELERATION = 1000  # mm/s^2
 MAX_ANGULAR_SPEED = 100  # rad/s
 MAX_ANGULAR_ACC = 3  # rad/s^2
 MIN_LINEAR_SPEED = 200  # mm/s Speed near zero, but still move the robot
@@ -13,8 +13,7 @@ MIN_LINEAR_SPEED = 200  # mm/s Speed near zero, but still move the robot
 
 class Robot:
 
-    __slots__ = ('_id', 'is_on_field', 'velocity_regulator', 'position_regulator', 'pose', 'velocity',
-                 'path', 'engine_cmd', 'target_speed', 'last_commanded_velocity')
+    __slots__ = ('_id', 'is_on_field', 'velocity_regulator', 'position_regulator', 'pose', 'velocity', 'path', 'engine_cmd', 'target_speed')
 
     def __init__(self, _id: int):
         self._id = _id
@@ -26,7 +25,6 @@ class Robot:
         self.engine_cmd = None
         self.target_speed = None
         self.is_on_field = False
-        self.last_commanded_velocity = None
 
     @property
     def id(self) -> int:
@@ -108,3 +106,4 @@ class Robot:
     def distance_to_path_end(self) ->Optional[float]:
         if self.path:
             return (self.position - self.path.target).norm
+
