30a5f359e8b7ab3df4f49a55ada72508252730f8
Simon Bouchard <simon.bouchard.9@ulaval.ca>
2018-06-14 14:11:47 -0400

    remove force stop at each path's points.

--- Engine/controller.py
+++ Engine/controller.py
@@ -59,9 +59,12 @@ class Controller:
         for robot in self.active_robots:
             robot.path, robot.target_speed = path_smoother(robot.raw_path, robot.cruise_speed, robot.end_speed)
 
-            commands[robot.id] = min(robot.velocity_regulator.execute(robot, dt),
-                                     robot.position_regulator.execute(robot, dt),
-                                     key=lambda cmd: cmd.norm)
+            if robot.target_speed < 10:
+                commands[robot.id] = min(robot.velocity_regulator.execute(robot, dt),
+                                         robot.position_regulator.execute(robot, dt),
+                                         key=lambda cmd: cmd.norm)
+            else:
+                commands[robot.id] = robot.velocity_regulator.execute(robot, dt)
 
         self.send_debug(commands)
 
