d383c64820bc9277971763de9afb35185a9e396d
philippe <philippe.lebel008@sympatico.ca>
2018-06-17 17:53:24 -0400

    eat this babin

--- Engine/robot.py
+++ Engine/robot.py
@@ -4,8 +4,8 @@ from Util import Pose, Position, Path
 from Util.geometry import wrap_to_pi
 
 MAX_LINEAR_SPEED = 4000  # mm/s
-MAX_LINEAR_ACCELERATION = 4000  # mm/s^2
-MAX_ANGULAR_SPEED = 50  # rad/s
+MAX_LINEAR_ACCELERATION = 3000  # mm/s^2
+MAX_ANGULAR_SPEED = 100  # rad/s
 MAX_ANGULAR_ACC = 3  # rad/s^2
 MIN_LINEAR_SPEED = 200  # mm/s Speed near zero, but still move the robot
 
