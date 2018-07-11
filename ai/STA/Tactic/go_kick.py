85a507a530a9213765208f0ed97329e8ca852c3a
Philippe Babin <philippe.babin@gmail.com>
2018-07-11 17:30:56 -0400

    WTF is this

--- ai/STA/Tactic/go_kick.py
+++ ai/STA/Tactic/go_kick.py
@@ -20,10 +20,10 @@ from ai.states.game_state import GameState
 VALIDATE_KICK_DELAY = 0.5
 TARGET_ASSIGNATION_DELAY = 1.0
 
-GO_BEHIND_SPACING = 180
-GRAB_BALL_SPACING = 90
+GO_BEHIND_SPACING = 250
+GRAB_BALL_SPACING = 120
 APPROACH_SPEED = 100
-KICK_DISTANCE = 90
+KICK_DISTANCE = 130
 KICK_SUCCEED_THRESHOLD = 300
 COMMAND_DELAY = 0.5
 
@@ -77,16 +77,17 @@ class GoKick(Tactic):
         ball_speed = self.game_state.ball.velocity.norm
         ball_speed_modifier = (ball_speed/1000 + 1)
         angle_behind = self.get_alignment_with_ball_and_target()
-        if angle_behind > 35:
-            effective_ball_spacing = GO_BEHIND_SPACING * min(3, abs(angle_behind/45)) * ball_speed_modifier
+        if angle_behind > 30:
+            effective_ball_spacing = GRAB_BALL_SPACING * min(2, abs(angle_behind/30)) * ball_speed_modifier
             collision_ball = True
         else:
-            effective_ball_spacing = GO_BEHIND_SPACING
+            effective_ball_spacing = GRAB_BALL_SPACING
             collision_ball = False
         distance_behind = self.get_destination_behind_ball(effective_ball_spacing)
         dist_from_ball = (self.player.position - self.game_state.ball_position).norm
-        if self.get_alignment_with_ball_and_target() < 25 \
-                and compare_angle(self.player.pose.orientation, orientation, abs_tol=max(0.05, 0.05 * dist_from_ball/1000)):
+
+        if self.get_alignment_with_ball_and_target() < 18 \
+                and compare_angle(self.player.pose.orientation, orientation, abs_tol=max(0.1, 0.1 * dist_from_ball/1000)):
             self.next_state = self.grab_ball
         else:
             self.next_state = self.go_behind_ball
@@ -94,12 +95,12 @@ class GoKick(Tactic):
                                       cruise_speed=3,
                                       end_speed=0,
                                       ball_collision=collision_ball)\
-                           .addChargeKicker().addKick(self.kick_force).build()
+                           .addChargeKicker().build()
 
     def grab_ball(self):
         if self.auto_update_target:
             self._find_best_passing_option()
-        if self.get_alignment_with_ball_and_target() > 45:
+        if self.get_alignment_with_ball_and_target() > 25:
             self.next_state = self.go_behind_ball
 
         if self._get_distance_from_ball() < KICK_DISTANCE:
@@ -107,10 +108,9 @@ class GoKick(Tactic):
             self.kick_last_time = time.time()
         ball_speed = self.game_state.ball.velocity.norm
         orientation = (self.target.position - self.game_state.ball_position).angle
-        spacing_offset = abs(1 - np.dot((self.player.position-self.target.position).array, (self.player.position-self.game_state.ball.position).array))
         distance_behind = self.get_destination_behind_ball(GRAB_BALL_SPACING)
         return CmdBuilder().addMoveTo(Pose(distance_behind, orientation),
-                                      cruise_speed=1,
+                                      cruise_speed=3,
                                       end_speed=0,
                                       ball_collision=False)\
                            .addForceDribbler()\
@@ -184,7 +184,7 @@ class GoKick(Tactic):
 
             self.target_assignation_last_time = time.time()
 
-    def get_destination_behind_ball(self, ball_spacing, velocity=True, velocity_offset=4) -> Position:
+    def get_destination_behind_ball(self, ball_spacing, velocity=True, velocity_offset=15) -> Position:
         """
          Compute the point which is at ball_spacing mm behind the ball from the target.
         """
@@ -214,7 +214,7 @@ class GoKick(Tactic):
                                    (normalize(self.player.position - self.game_state.ball_position)).array)
         return np.arccos(alignement_behind) * 180 / np.pi
 
-    def debug_cmd_fuck(self):
+    def debug_cmd(self):
         angle = None
         additional_dbg = []
         if self.current_state == self.go_behind_ball:
