dac1fc33c7708dfdef55d29e1fa3df842a1903bc
wonwon0 <philippe.lebel.proton@protonmail.com>
2019-03-14 19:57:16 -0400

    modification path_smoother

--- Engine/Controller/path_smoother.py
+++ Engine/Controller/path_smoother.py
@@ -12,19 +12,45 @@ def path_smoother(path, speed, end_speed) -> Tuple[Path, float]:
     if len(path) < 3:
         return path, end_speed
 
-    p1 = path[0]
-    p2 = path[1]
-    p3 = path[2]
+    number_of_turns = len(path.points) - 2
+    next_speeds = []
+    next_turn_point_list = path
+    for i in range(number_of_turns):
+        p1 = path[i]
+        p2 = path[i+1]
+        p3 = path[i+2]
 
-    p4, p5 = compute_circle_points(p1, p2, p3, speed, acc=MAX_LINEAR_ACCELERATION)
+        p4, p5 = compute_circle_points(p1, p2, p3, speed, acc=MAX_LINEAR_ACCELERATION)
 
-    if (p1 - p2).norm < (p4 - p2).norm or (p3 - p2).norm < (p5 - p2).norm:
-        point_list = path[:3]
-    else:
-        point_list = [path.start, p4, p5]
-    turn_radius, _ = compute_turn_radius(*point_list, speed, acc=MAX_LINEAR_ACCELERATION)
-    next_speed = speed_in_corner(turn_radius, acc=MAX_LINEAR_ACCELERATION)
-    return Path.from_sequence(point_list), next_speed
+        if (p1 - p2).norm < (p4 - p2).norm or (p3 - p2).norm < (p5 - p2).norm:
+            point_list = path[:3]
+        else:
+            point_list = [path.start, p4, p5]
+        if i == 0:
+            next_turn_point_list = point_list
+        turn_radius, _ = compute_turn_radius(*point_list, speed, acc=MAX_LINEAR_ACCELERATION)
+        next_speed = speed_in_corner(turn_radius, acc=MAX_LINEAR_ACCELERATION)
+        next_speeds.append(next_speed)
+    retries = 0
+    i = 0
+    while i < len(next_speeds)-1:
+        if retries > 10:
+            next_speeds[0] = 0
+            break
+        initial_speed = next_speeds[i]
+        final_speed = next_speeds[i+1]
+        if final_speed > initial_speed:
+            pass
+        else:
+            delta_position = (path.points[i+1]-path.points[i+2]).norm
+            if not is_time_to_break(initial_speed, final_speed, delta_position):
+                next_speeds[i] = get_good_speed_to_break(final_speed, delta_position)
+                i = 0
+                retries += 1
+
+        i += 1
+
+    return Path.from_sequence(next_turn_point_list), next_speeds[0]
 
 
 def compute_circle_points(p1, p2, p3, speed: float, acc: float) -> Tuple[Position, Position]:
@@ -77,3 +103,13 @@ def deviation(radius: float, theta: float) -> float:
 def point_on_segment(start: Position, end: Position, distance: float):
     ratio = distance / (start-end).norm
     return start * (1-ratio) + end * ratio
+
+
+def is_time_to_break(initial_speed, final_speed, delta_positon):
+    return delta_positon < ((final_speed ** 2 - initial_speed ** 2) / 2 * MAX_LINEAR_ACCELERATION)
+
+
+def get_good_speed_to_break(final_speed, delta_position):
+    if final_speed ** 2 < 2 * MAX_LINEAR_ACCELERATION * delta_position:
+        return 0
+    return sqrt(final_speed ** 2 - 2 * MAX_LINEAR_ACCELERATION * delta_position)
