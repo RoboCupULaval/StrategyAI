47a6245f2c65ac62b6519f00bf45af5ac5713aae
wonwon0 <philippe.lebel.proton@protonmail.com>
2019-03-18 18:57:52 -0400

    modif go_kick pour le comment a babin

--- ai/STA/Tactic/go_kick.py
+++ ai/STA/Tactic/go_kick.py
@@ -80,7 +80,6 @@ class GoKick(Tactic):
 
         required_orientation = (self.target.position - self.game_state.ball_position).angle
         ball_speed = self.game_state.ball.velocity.norm
-        ball_speed_modifier = (ball_speed/1000 + 1)
         angle_behind = self.get_alignment_with_ball_and_target()
         if angle_behind > 35:
             effective_ball_spacing = GO_BEHIND_SPACING
@@ -88,10 +87,8 @@ class GoKick(Tactic):
         else:
             effective_ball_spacing = GO_BEHIND_SPACING
             collision_ball = False
-            if self.get_alignment_with_ball_and_target() < 35 \
-                    and compare_angle(self.player.pose.orientation,
-                                      required_orientation,
-                                      abs_tol=max(0.05, 0.05 * dist_from_ball / 1000)):
+            if compare_angle(self.player.pose.orientation, required_orientation,
+                             abs_tol=max(0.05, 0.05 * dist_from_ball / 1000)):
                 self.next_state = self.grab_ball
             else:
                 self.next_state = self.go_behind_ball
